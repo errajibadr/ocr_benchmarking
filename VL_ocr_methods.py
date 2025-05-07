9832f9de9d99d1608b26dc8262fd198fdc975aca
errajibadr <errajibadr95@gmail.com>
2025-05-07 11:49:50 +0200

    Add ipykernel dependency to pyproject.toml and uv.lock. Update ColQwen2 model version in VL_ocr_methods.py. Replace hardcoded image paths with dynamic loading from dataset. Remove outdated extracted text files for EasyOCR, PaddleOCR, and Tesseract.

--- VL_ocr_methods.py
+++ VL_ocr_methods.py
@@ -1,26 +1,27 @@
 ## git+https://github.com/illuin-tech/colpali and torchvision
 
+from pathlib import Path
+from typing import cast
+
 import torch
 from colpali_engine.models import ColQwen2, ColQwen2Processor
 from PIL import Image
 from transformers.utils.import_utils import is_flash_attn_2_available
 
 model = ColQwen2.from_pretrained(
-    "vidore/colqwen2-v0.1",
+    "vidore/colqwen2-v1.0",
     torch_dtype=torch.bfloat16,
     device_map="mps",  # "cuda:0",  # or "mps" if on Apple Silicon
     attn_implementation="flash_attention_2" if is_flash_attn_2_available() else None,
 ).eval()
-processor = ColQwen2Processor.from_pretrained("vidore/colqwen2-v1.0")
+processor = cast(ColQwen2Processor, ColQwen2Processor.from_pretrained("vidore/colqwen2-v1.0"))
 
 # Your inputs
-images = [
-    Image.new("RGB", (128, 128), color="white"),
-    Image.new("RGB", (64, 32), color="black"),
-]
+images = [Image.open(str(image)) for image in Path("dataset/sample/images").glob("*.png")]
+# images = [Image.open("dataset/sample/images/82200067_0069.png")]
+images = images[:3]
 queries = [
-    "Is attention really all you need?",
-    "What is the amount of bananas farmed in Salvador?",
+    "guidelines for advertising",
 ]
 
 # Process the inputs
